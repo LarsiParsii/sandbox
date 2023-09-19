ac1adb1683b15ce1432d85e16f588465f1c9cdc4
Lars Fredrik <lars-on@hotmail.com>
2023-09-20 00:02:50 +0200

    Fuck this shit. Can't wrap my head around the logic.

--- calculator/app.js
+++ calculator/app.js
@@ -18,22 +18,45 @@ let total = 0;
 let displayed_num = "0";
 display.innerHTML = displayed_num;
 let dec_used = false;
-let new_input = false;
+let start_new_input = true;
+let prev_number = 0;
 let prev_operation = NONE;
 
 num_buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
     const value = event.target.textContent;
 
-    if (new_input == false) {
+    if (start_new_input == true) {
       displayed_num = value;
-      new_input = true;
+      start_new_input = false;
     } else {
-      displayed_num += value;
+      if (displayed_num !== "0") {
+        displayed_num += value;
+      }
     }
-
     display.innerHTML = displayed_num;
   });
+});
+
+add_button.addEventListener("click", (event) => {
+
+});
+
+sub_button.addEventListener("click", (event) => {
+
+});
+
+clr_button.addEventListener("click", (event) => {
+  dec_used = false;
+  start_new_input = true;
+  prev_operation = NONE
+  total = 0;
+  displayed_num = "0";
+  display.innerHTML = displayed_num;
+});
+
+eql_button.addEventListener("click", (event) => {
+  run_last_operation();
 });
 
 dec_button.addEventListener("click", (event) => {
@@ -44,60 +67,25 @@ dec_button.addEventListener("click", (event) => {
   }
 });
 
-clr_button.addEventListener("click", (event) => {
-  dec_used = false;
-  new_input = false;
-  prev_operation = NONE;
-  total = 0;
-  displayed_num = "0";
-  display.innerHTML = displayed_num;
-});
-
-eql_button.addEventListener("click", (event) => {
-  console.log(`Before =: ${total}.`);
+const run_last_operation = () => {
   switch (prev_operation) {
+    case NONE:
+      break;
     case ADD:
-      total += parseFloat(displayed_num);
+      total += prev_number;
       break;
     case SUB:
-      total -= parseFloat(displayed_num);
+      total -= prev_number;
       break;
-    case MUL:
-      break;
-    case DIV:
+    default:
+      console.error("This should never occur.");
       break;
   }
 
-  displayed_num = total;
-  new_input = false;
-
-  display.innerHTML = displayed_num;
-  console.log(`After  =: ${total}.`);
-});
-
-add_button.addEventListener("click", (event) => {
-  console.log(`Before +: ${total}.`);
-  if (new_input) {
-    total += parseFloat(displayed_num);
+  if (prev_operation !== NONE) {
     displayed_num = total;
-    prev_operation = ADD;
-    new_input = false;
     display.innerHTML = displayed_num;
   }
-  console.log(`After  +: ${total}.`);
-});
-
-sub_button.addEventListener("click", (event) => {
-  console.log(`Before -: ${total}.`);
-
-  total += parseFloat(displayed_num);
-
-  if (new_input) {
-    total -= parseFloat(displayed_num);
-    displayed_num = total;
-    prev_operation = SUB;
-    new_input = false;
-    display.innerHTML = displayed_num;
-  }
-  console.log(`After  -: ${total}.`);
-});
+  prev_operation = NONE;
+  start_new_input = true;
+};
